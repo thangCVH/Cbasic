7f93ffd474ce6259f06f3576125d796ac5014979
thangtaide <63969960+thangtaide@users.noreply.github.com>
2022-10-12 13:21:57 +0700

    Update 2.4c

--- Lab02/2-4b.c
+++ Lab02/2-4b.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+int multiplication(int n){
+    if(n<1) return 0;
+    else if(n = 1) return 1;
+    else if(n = 2) return 2;
+    else return n*mul(n-2);
+    
+}
+
 int main()
 {
-    int n,mul=1,m;
+    int n,m;
     printf("input n\n");
     scanf("%d",&n);
-    if (n%2==0)
-    {
-        for (m=2;m<=n;m+=2)
-        {
-            mul=mul*m;
-        }
-        printf("multiplication=%d",mul);
-    }
-    if (n%2==1)
-    {
-        for (m=1;m<=n;m+=2)
-        {
-            mul=mul*m;
-        }
-        printf("multiplication=%d",mul);
-        
-    }
+        printf("multiplication=%d",multiplication(n));
     return 0;
 }
